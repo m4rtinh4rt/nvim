180ee172195890202bcab67154cf96be393f0c7c
Martin Hart <114212671+m4rtinh4rt@users.noreply.github.com>
2024-09-28 21:49:12 +0200

    Remove some more comments

--- init.lua
+++ init.lua
@@ -176,21 +176,6 @@ require('lazy').setup({
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in Telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
-
       require('telescope').setup {
         --  All the info you're looking for is in `:help telescope.setup()`
         --
